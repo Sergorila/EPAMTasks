b6299d9d97e0ddb6023266f127b143732c8a4b34
Sergorila <sid0rov.sa@yandex.ru>
2022-01-10 14:33:56 +0400

    Change the split sentences

--- Task 1/Task 1.2/Task 1.2.4/Validator/Program.cs
+++ Task 1/Task 1.2/Task 1.2.4/Validator/Program.cs
@@ -5,43 +5,6 @@ namespace Validator
 {
     class Program
     {
-        public static string Validator(string s)
-        {
-            string sep = ".!?";
-
-            string temp = s;
-            for (int i = 0; i < temp.Length; i++)
-            {
-                if (sep.Contains(temp[i]))
-                {
-                    temp = temp.Insert(i + 1, "+");
-                }
-            }
-
-            string[] words = temp.Split('+');
-
-            for (int i = 0; i < words.Length; i++)
-            {
-                words[i] = words[i].Trim();
-                if (words[i].Length > 1)
-                {
-                    words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1, words[i].Length - 1);
-                }
-                else
-                {
-                    words[i].ToUpper();
-                }
-            }
-
-            string res = "";
-            foreach (var word in words)
-            {
-                res += word;
-                res += " ";
-            }
-
-            return res;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter text");
@@ -49,6 +12,47 @@ namespace Validator
 
             Console.WriteLine("Result text");
             Console.WriteLine(Validator(s));
+        }
+
+        public static StringBuilder Validator(string s)
+        {
+            string sep = ".!?";
+
+            string[] words = s.Split(' ');
+
+            bool isUpperSymbol = true;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (isUpperSymbol)
+                {
+                    if (words[i].Length > 1)
+                    {
+                        words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1, words[i].Length - 1);
+                    }
+                    else
+                    {
+                        words[i] = words[i].ToUpper();
+                    }
+
+                    isUpperSymbol = false;
+                }
+
+                if (sep.Contains(words[i][words[i].Length - 1]))
+                {
+                    isUpperSymbol = true;
+                }
+                
+            }
+
+            StringBuilder res = new StringBuilder("");
+            foreach (var word in words)
+            {
+                res.Append(word);
+                res.Append(" ");
+            }
+
+            return res;
         }
     }
 }
