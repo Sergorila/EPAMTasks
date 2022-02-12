3e7cd190e2f446d7a5c4e2dc27972fcd52cd32f1
Sergorila <sid0rov.sa@yandex.ru>
2022-02-12 21:47:48 +0400

    Add solvation of Task3.2

---
+++ Task3/Task3.2/Task3.2.1/DynamicArray/DynamicArray/Array/DynamicArrayEnumerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    public class DynamicArrayEnumerator<T> : IEnumerator<T>
+    {
+        protected T[] _array;
+        protected int _index = -1;
+
+        public T Current => _array[_index];
+
+        object IEnumerator.Current => _array[_index];
+
+        public DynamicArrayEnumerator(T[] array)
+        {
+            _array = array;
+        }
+
+        public bool MoveNext()
+        {
+            if (_index != _array.Length - 1)
+            {
+                _index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            _index = 0;
+        }
+
+        public void Dispose()
+        {
+            _index = -1;
+        }
+    }
+}
